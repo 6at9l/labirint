e33fe64263074ba15adfba4982ce89ecb68c0ab7
Sergey <6at9lodi@gmail.com>
2017-06-13 14:22:03 +0300

    last master  change

--- src/labirint/worker.js
+++ src/labirint/worker.js
@@ -16,8 +16,22 @@ function check(){
 var data = {};
 
 var strData = "var data = {};this.addEventListener('message', function(e){data = e.data;});";
+var text = `
+    function setDir(d){
+      data = {finish: false};
+      this.postMessage({"action": "changeDirection", "d": d});
+    }
 
+    function move(){
+      data = {finish: false};
+      this.postMessage({"action": "move"});
+    }
 
+    function check(){
+      data = {finish: false};
+      this.postMessage({"action": "check"});
+    }`;
+strData += text;
 export default {setDir, move, check, strData};
 
 
